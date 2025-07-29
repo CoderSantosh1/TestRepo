c20df1af5b1c15d51c1be2a700b73ed69d72f979
santoshcoder <santoshcoderr@gmail.com>
2025-07-29 19:06:45 +0530

    chnage anylatics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from "next";
 import { Montserrat } from "next/font/google";
 import { ThemeProvider } from "@/components/theme-provider";
 import "./globals.css";
-import Script from 'next/script';
-import { SpeedInsights } from '@vercel/speed-insights/react';
-import { Analytics } from '@vercel/analytics/react';
+import Script from "next/script";
+import { SpeedInsights } from "@vercel/speed-insights/react";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Montserrat({
   subsets: ["latin"],
@@ -13,35 +13,62 @@ const inter = Montserrat({
 
 export const metadata: Metadata = {
   title: "Sarkariresultsnow.com",
-  description: "SarkariResultsNow provides all Sarkari Result, Admit Card, Government Jobs, Free Test Series Also Available",
+  description:
+    "SarkariResultsNow provides all Sarkari Result, Admit Card, Government Jobs, Free Test Series Also Available",
 };
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
-        <meta name="google-site-verification" content="KpO7IT-JrAfXS5vKDai0XcwaVUS-JzRDwsl1kBtfoyI" />
+        <meta
+          name="google-site-verification"
+          content="KpO7IT-JrAfXS5vKDai0XcwaVUS-JzRDwsl1kBtfoyI"
+        />
         <meta name="google-adsense-account" content="ca-pub-7649078598124252" />
         <link rel="icon" type="image/png" href="/logo-color-new.jpg" />
-        <meta property="og:title" content="Sarkariresultsnow - Latest Govt Job Results, Admit Cards & News" />
-        <meta property="og:description" content="Get the latest updates on government job results, admit cards, sarkari news, and more. Stay informed with Sarkariresultsnow for all your govt job needs." />
-        <meta property="og:image" content="https://www.sarkariresultsnow.com/og-image.png" />
+        <meta
+          property="og:title"
+          content="Sarkariresultsnow - Latest Govt Job Results, Admit Cards & News"
+        />
+        <meta
+          property="og:description"
+          content="Get the latest updates on government job results, admit cards, sarkari news, and more. Stay informed with Sarkariresultsnow for all your govt job needs."
+        />
+        <meta
+          property="og:image"
+          content="https://www.sarkariresultsnow.com/og-image.png"
+        />
         <meta property="og:url" content="https://www.sarkariresultsnow.com/" />
-        <meta name="description" content="SarkariResultsNow provides all Sarkari Result, Admit Card, Government Jobs and Free Test Series , Latest Notifications." />
-       
+        <meta
+          name="description"
+          content="SarkariResultsNow provides all Sarkari Result, Admit Card, Government Jobs and Free Test Series , Latest Notifications."
+        />
+
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-8GZPT68Z06"
+        ></script>
+        <script>
+          (window as any).dataLayer = (window as any).dataLayer || []; function
+          gtag(){(window as any).dataLayer.push(arguments)}
+          gtag('js', new Date()); gtag('config', 'G-8GZPT68Z06');
+        </script>
       </head>
-      <body className={`${inter.className} antialiased`} suppressHydrationWarning>
+      <body
+        className={`${inter.className} antialiased`}
+        suppressHydrationWarning
+      >
         <ThemeProvider
           attribute="class"
           defaultTheme="light"
           enableSystem
           disableTransitionOnChange
         >
-        
           {children}
           <SpeedInsights />
           <Analytics />
